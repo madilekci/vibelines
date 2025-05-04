a3664e1ef31b62f87dd38d0969e3f2d736867c38
Muhammed Ali <160202093@kocaeli.edu.tr>
2025-05-04 03:27:00 +0300

    Add bootstrap with cdn instead

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [QuoteController::class, 'index'])->name('quotes.index');
-Route::post('/quotes', [QuoteController::class, 'store'])->name('quotes.store');
+Route::get('/', [QuoteController::class, 'showRandomQuote'])->name('home');
+
+// group for quotes
+Route::prefix('quotes')->name('quotes.')->group(function () {
+    // Route::get('/', [QuoteController::class, 'index'])->name('index');
+    Route::post('/', [QuoteController::class, 'store'])->name('store');
+    Route::get('/submit', [QuoteController::class, 'create'])->name('create');
+    Route::post('/submit', [QuoteController::class, 'store'])->name('store');
+});
 
 // {quote} here should be the ID of the quote.
 // Laravel will automatically resolve it to the Quote model instance and pass it to the controller method.
-Route::post('/quotes/{quote}/react', [ReactionController::class, 'store'])->name('reactions.store');
+Route::post('/{quote}/react', [ReactionController::class, 'store'])->name('reactions.store');
 
 
 // route group for admin routes
-Route::group(['prefix' => 'admin',], function () {
-    Route::get('/quotes', [QuoteController::class, 'adminIndex'])->name('quotes.admin');
-    Route::post('/quotes/{quote}/toggle', [QuoteController::class, 'toggleApproval'])->name('quotes.toggleApproval');
+Route::prefix('admin')->name('admin.')->group( function () {
+    Route::get('/quotes', [QuoteController::class, 'adminIndex'])->name('quotes.index');
+    Route::post('/quotes/{quote}/toggle', [QuoteController::class, 'toggleApproval'])->name('quotes.toggle');
     Route::delete('/quotes/{quote}', [QuoteController::class, 'destroy'])->name('quotes.destroy');
+});
+
+Route::fallback(function () {
+    return redirect()->route('home');
 });
